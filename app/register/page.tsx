e258ed89df92ca01031495b9455323c44090a3df
Farhan Jawad <farhanjawad@yandex.com>
2026-05-10 00:11:43 +0600

    tailwind latest

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -370,7 +370,7 @@ const [showPaymentModal, setShowPaymentModal] = useState(false);
       {/* PAYMENT INSTRUCTION MODAL */}
       {/* ========================================== */}
       {showPaymentModal && (
-        <div className="fixed inset-0 z-[100] flex items-center justify-center bg-black/60 backdrop-blur-sm p-4 animate-in fade-in duration-300 font-kalpurush">
+        <div className="fixed inset-0 z-100 flex items-center justify-center bg-black/60 backdrop-blur-sm p-4 animate-in fade-in duration-300 font-kalpurush">
           <div className="bg-white rounded-2xl shadow-2xl max-w-md w-full p-6 animate-in zoom-in-95 duration-300 relative border border-emerald-100">
             
             {/* Close Button (Top Right) */}
